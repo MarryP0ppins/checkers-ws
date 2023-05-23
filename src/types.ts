93f06d4bd1cdebe01ae8bfbb0e10f96c5e120d42
Iana Stelmakh <stelmakyana99@gmail.com>
2023-05-24 00:21:35 +0300

    types

--- src/types.ts
+++ src/types.ts
@@ -8,6 +8,15 @@ export interface JoinGameRequest {
     gameId: number;
 }
 
+export interface MakeMoveRequest {
+    game: number;
+    user: number;
+    checker_id: number;
+    new_positions: string[];
+    is_king: boolean;
+    is_white: boolean;
+}
+
 export interface OpenGamesProps {
     id: number;
     socketId: string;
@@ -26,6 +35,7 @@ export interface GameListData {
 export interface ClientToServerEvents {
     createGameRequest: (message: CreateGameRequest, callback: (gameId: number) => void) => void;
     joinGame: (message: JoinGameRequest) => void;
+    playerMakeMove: (message: MakeMoveRequest) => void;
 }
 
 export interface ServerToClientEvents {
